8fa619daa84a7354421c401527024938b4966b43
landylan <landylan.163@gmail.com>
2017-10-27 22:40:40 +0800

    Javascript 變更標題

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,3 @@
 d3.json('/static/data/data.json', function(error, data){
-  d3.select("h1#H1_title").text('===  這是由 Javascript 變更過的標題  ===')
+  d3.select("h1#H1_title").text('=== Title from Javascript ===');
 });
