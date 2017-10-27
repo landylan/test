5a61e93baccbea63232fe46c98f90aa33b38f5fd
landylan <landylan.163@gmail.com>
2017-10-27 18:10:17 +0800

    php 也要設定好中文編碼

--- main.php
+++ main.php
@@ -1,1 +1,8 @@
-<?php print "這是由 php 的 print 輸出的一段文字。。。"; ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1><?php print "這是由 php 的 print 輸出的一段文字。。。"; ?></h1>
+    </body>
+</html>
