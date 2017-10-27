acf18a88ae780e734ee29f8e00f2637954af6eaa
landylan <landylan.163@gmail.com>
2017-10-27 19:27:52 +0800

    render_template 也要 import 進來，還是不行。。。 ==》再把 appengine_config.py 拉進來，並將 main.py 中的 app.run 刪除。。。

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
   return  render_template("index.tpl.html", heading="這是由 Flask 的 render_template 丟出來的一個頁面。。。")
-  
-if __name__ == '__main__':
-  app.run()
